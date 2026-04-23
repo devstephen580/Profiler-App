9971022557c87335da3391d54e18a75f7f7d780e
devstephen580 <stephendev580@gmail.com>
2026-04-23 12:09:24 +0100

    feature: added cors configuration

--- src/main/java/com/devStephen/profiler_api/configuration/Config.java
+++ src/main/java/com/devStephen/profiler_api/configuration/Config.java
@@ -2,16 +2,33 @@ package com.devStephen.profiler_api.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
 
 
 @Configuration
-public class WebClientConfig {
+public class Config {
 
     @Bean
     public WebClient webClient() {
         return WebClient.builder().build();
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.addExposedHeader("Access-Control-Allow-Origin");
 
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
 }
